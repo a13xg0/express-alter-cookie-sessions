25d72d163596be32a5a7b19dc2bb06527f676d55
Alex <a.v.gorbach@gmail.com>
2024-04-16 07:39:38 +0200

    add express with the index page rendering

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world!')
+import express from 'express'
+import * as path from 'node:path'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
