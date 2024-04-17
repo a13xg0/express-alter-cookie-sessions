388719e5d766a47dd9c545a52752d5c11f0ed891
Alex <a.v.gorbach@gmail.com>
2024-04-17 14:50:23 +0200

    add passport with cookie-session

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,43 @@
 import express from 'express'
+import cookieSession from 'cookie-session'
 import * as path from 'node:path'
+import bodyParser from 'body-parser'
+import passport from './passport'
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(
+  cookieSession({
+    secret: 'super secret key',
+    name: 'sessions',
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    secure: false,
+    keys: ['Qjkdffks', 'KJKJSDrefjk3jk'],
+  }),
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post(
+  '/login',
+
+  passport.authenticate('local', {
+    failureRedirect: '/',
+  }),
+  (req, res) => {
+    res.status(200).send('logged in!')
+  },
+)
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
---
+++ src/passport.ts
@@ -0,0 +1,31 @@
+import { Strategy } from 'passport-local'
+import passport from 'passport'
+import { users } from './models/users'
+
+passport.use(
+  new Strategy((username, password, done) => {
+    console.log('username', username)
+    const user = users.find((user) => user.email === username)
+    if (!user) {
+      return done(null, false)
+    }
+
+    const valid = password === user.password
+    if (!valid) {
+      return done(null, false, { message: 'Incorrect username or password.' })
+    }
+
+    return done(null, user)
+  }),
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+  const user = users.find((u) => u.id === id)
+  done(null, user)
+})
+
+export default passport
